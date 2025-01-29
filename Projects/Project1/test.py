ae1302f0b2da309f80afee9169c8a1d7fb912d18
Paul Tuccinardi <47836074+PTucc327@users.noreply.github.com>
2025-01-29 20:23:25 +0000

    changes to project 1

--- Projects/Project1/test.py
+++ Projects/Project1/test.py
@@ -4,7 +4,19 @@ convert string to url
 generate score based on rules
 return json object including score and explanation of the score
 '''
+import json
+from urllib.parse import quote
+
+def eval_url(url) :
+    ''' takes in a url and 
+    '''
+    new_url = quote(url)
+    json_output = '{"Score: " 0, "Explanation: " "This is why"}'
 
 
+    return json_output
 
 
+var1 = input("Enter a URL:" )
+
+print(eval_url(var1))
