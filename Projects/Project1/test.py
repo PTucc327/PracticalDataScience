43788550b08b672ddf3990d33542e51cff5aa45d
Paul Tuccinardi <47836074+PTucc327@users.noreply.github.com>
2025-01-30 15:03:11 +0000

    adding in url related code

--- Projects/Project1/test.py
+++ Projects/Project1/test.py
@@ -5,18 +5,31 @@ generate score based on rules
 return json object including score and explanation of the score
 '''
 import json
-from urllib.parse import quote
+import requests
+from bs4 import BeautifulSoup
 
 def eval_url(url) :
     ''' takes in a url and 
     '''
-    new_url = quote(url)
+    response = requests.get(url)
+    #Check if the url is valid or not
+    if response.status_code == 200:
+        html_content = response.content
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+    #Extract information from url
+    soup = BeautifulSoup(html_content, "html.parser")
+    paragraphs = soup.find_all("p", class_="article-text")
+    for paragraph in paragraphs:
+        print(paragraph.text)
+    
+    #Create JSON object to output
     json_output = '{"Score: " 0, "Explanation: " "This is why"}'
-
+    
 
     return json_output
 
 
-var1 = input("Enter a URL:" )
+var1 = input("Enter a URL: " )
 
 print(eval_url(var1))
